65b3e92e8c9f436e2c371e52de223dfd80d0103f
Varun <cyberslicks@gmail.com>
2012-09-17 00:55:16 +0530

    login n signup with password strength

---
+++ varun_rocks/app page_layout/passcheck.js
@@ -0,0 +1,26 @@
+function passwordStrength(password,passwordStrength,errorField)
+{
+var desc = new Array();
+desc[0] = "Very Weak";
+desc[1] = "Weak";
+desc[2] = "Better";
+desc[3] = "Medium";
+desc[4] = "Strong";
+desc[5] = "Strongest";
+
+var score   = 0;
+
+if (password.length > 6) score++;
+
+if ( ( password.match(/[a-z]/) ) && ( password.match(/[A-Z]/) ) ) score++;
+
+if (password.match(/\d+/)) score++;
+
+if ( password.match(/.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/) ) score++;
+
+if (password.length > 12) score++;
+
+passwordStrength.innerHTML = desc[score];
+passwordStrength.className = "strength" + score;
+}
+
