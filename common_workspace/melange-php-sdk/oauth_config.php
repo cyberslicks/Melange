7a509b47d702f2e26c92da9a42ef2a6d42ba7f6d
psych0der <nobles0ul@me.com>
2012-10-02 01:53:12 +0530

    google login added

--- common_workspace/melange-php-sdk/oauth_config.php
+++ common_workspace/melange-php-sdk/oauth_config.php
@@ -1,19 +1,26 @@
 <?php
 	
+
+/*  host url variables */
+
+$_host = $_SERVER['HTTP_HOST'];
+$_request_url = $_SERVER['REQUEST_URI'];
+$_path = dirname($_host.$_request_url);
+
 /* Array set of api keys */
 
 $apiData = array(
 					
-					"facebook" => array("appId" =>"350137395076209" , "secret" => "f9c067ec2ddd523a68c30203274098c3")
+					"facebook" => array("appId" =>"350137395076209" , "secret" => "f9c067ec2ddd523a68c30203274098c3"),
+					"google" => array("clientId" => '507080955983.apps.googleusercontent.com' , "clientSecret" =>'aByFa_syiw4tqkte0bwBQhye',
+									  "redirectUrl" => 'http://localhost:8888/php_sandbox/melange-php-sdk/oAuthtester.php' , "developerKey" => 'AIzaSyBRKs9a_IoJzcucY3SPyQDWsXK4rO0drjI8')
 
 
 				);
 				
 		
-	/*  host url variables */
-	$_host = $_SERVER['HTTP_HOST'];
-	$_request_url = $_SERVER['REQUEST_URI'];
-	$_path = dirname($_host.$_request_url);
+	
+				//'http://'.$_path.'/oAuthtester.php'
 
 
 
--- psych0der_space/melange-php-sdk/oAuthInterface.php
+++ psych0der_space/melange-php-sdk/oAuthInterface.php
@@ -2,6 +2,8 @@
 	
 
 require './base_sdk/facebook.php';
+require_once './base_sdk/apiClient.php';
+require_once './base_sdk/contrib/apiOauth2Service.php';
 
 
 
@@ -12,6 +14,7 @@ class oAuthLogin
 	
 	private $facebook;
 	private $google;
+	private $goauth;
 	private $twitter;
 	private $user;
 	private $status = 0;
@@ -19,14 +22,9 @@ class oAuthLogin
 	private $_hostname = null;
 	private $_request_url = null;
 	
-	//global $_SERVER;
-	//echo $_SERVER;
-	// $_request_url = $_SERVER['REQUEST_URI'];
-	 //$_path = dirname($_host.$_request_url);
-	//echo $_path;
+	
 	
 	 			
-	
 	function __construct() 
 	{
 		include 'oauth_config.php';
@@ -35,10 +33,17 @@ class oAuthLogin
 	  	  	'secret' => $apiData['facebook']['secret'],
 			));
 		
-			$this->_hostname = $_SERVER['HTTP_HOST'];
-			//echo $this->_hostname;
+		
+		
+		$this->google  = new apiClient();
+		$this->google->setApplicationName("Cyber Slick");
+		$this->google->setClientId($apiData['google']['clientId']);
+		$this->google->setClientSecret($apiData['google']['clientSecret']);
+		$this->google->setRedirectUri($apiData['google']['redirectUrl']);
+		$this->google->setDeveloperKey($apiData['google']['developerKey']);
+		
 			
-			//echo $_SERVER['HTTP_HOST'] ."+". $_SERVER['REQUEST_URI'];
+			
 		$this->checkEstablishedSession();
 	
 	}
@@ -49,6 +54,57 @@ class oAuthLogin
 		$this->user = $this->facebook->getUser(); 		// fetches the status
 		if($this->user)
 			$this->status = 1;
+		
+		
+		
+	}
+	
+	public function googleSessionManager() 
+	{
+		include 'oauth_config.php';
+		$this->goauth = new apiOauth2Service($this->google);
+		
+		if (isset($_GET['code'])) {
+			
+		  $this->google->authenticate();
+		  
+		  $_SESSION['token'] = $this->google->getAccessToken();
+		  $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+		 // header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
+		}
+
+		if (isset($_SESSION['token'])) {
+			echo "<h1>lol</h1>";
+		 $this->google->setAccessToken($_SESSION['token']);
+		}
+
+		if (isset($_REQUEST['logout'])) {
+		  unset($_SESSION['token']);
+		  $this->google->revokeToken();
+		}
+
+		if ($this->google->getAccessToken()) {
+		  $user = $this->goauth->userinfo->get();
+		 
+		  $email = filter_var($user['email'], FILTER_SANITIZE_EMAIL);
+		  $img = filter_var($user['picture'], FILTER_VALIDATE_URL);
+		  $usr = filter_var($user['name'], FILTER_VALIDATE_URL);
+		  $personMarkup = "$email<div><img src='$img?sz=50'></div>";
+		  echo $email;
+		  echo "<br>".$user['name'];
+		  
+		  $this->status = 2;
+		  $this->printGOAuthData($personMarkup);
+		  // The access token may have been updated lazily.
+		  $_SESSION['token'] = $this->google->getAccessToken();
+		  echo "     ";
+		  print "<a class='logout' href=\"".'http://localhost:8888/php_sandbox/melange-php-sdk/logout.php'."\">google Logout</a>";
+		} else {
+		  $authUrl = $this->google->createAuthUrl();
+		  print "<a class='login' href='$authUrl'>gConnect Me!</a>"; 
+		  
+		  
+		}
 		
 		
 		
@@ -82,7 +138,10 @@ class oAuthLogin
 			
 		}
 		
-		else if($this->status == 0)
+		if($this->status ==0)
+			$this->googleSessionManager();
+		
+		 if($this->status == 0)
 		{
 			$url = $this->facebook->getLoginUrl();
 			$this->printLoginButton($url);
@@ -121,6 +180,13 @@ class oAuthLogin
       <img src=\"https://graph.facebook.com/".$this->user."/picture\">";
 	  echo "<br><br>";
 	  echo "<ul><li>".$this->user_profile["name"]."</li><li>".$this->user_profile["username"]."</li></ul>";
+		
+	}
+	
+	public function printGOAuthData($data)
+	{
+		echo "</br></br";
+		print $data;
 		
 	}
 	
