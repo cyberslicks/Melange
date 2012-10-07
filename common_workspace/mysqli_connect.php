9b0e1e0277cb96ebf93a3219835e78e0bf22d139
Cyber Slicks <cyberslicks@gmail.com>
2012-10-08 01:15:30 +0530

    login.php

--- common_workspace/mysqli_connect.php
+++ common_workspace/mysqli_connect.php
@@ -11,6 +11,6 @@ DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'melange');
 
 // Make the connection:
-$dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
+$dbc = mysql_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysql_error() );
 
 ?>
