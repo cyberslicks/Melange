68716120f012dda74da7491e4b6a29610b25e6de
Cyber Slicks <cyberslicks@gmail.com>
2012-10-08 21:48:05 +0530

    Final_Site....dont make changes inside git :p

---
+++ varun_rocks/Final_Site/signup/useravail.php
@@ -0,0 +1,26 @@
+<?php
+$q=$_GET["username"];
+
+$con = mysql_connect('localhost', 'root', '');
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("melange", $con);
+		$q = "SELECT username FROM users where username='$q'";		
+		$r = mysql_query ($q); 
+
+
+
+
+$row = mysql_fetch_array($r);
+  if($row['username'])
+	echo "not available";
+	 
+  else
+	echo "available";
+  
+
+mysql_close($con);
+?>
